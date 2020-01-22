bf998281a5c3e74f7549b10738a7507e3bd4278e
Jesus Renero <jesus.renero@gmail.com>
2020-01-22 14:47:50 +0100

    modified YAML for travis and readthedocs

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name='dataset',
     version='0.15',
-    packages=[''],
+    packages=['dataset'],
     url='https://github.com/renero/class_notebooks/tree/master/src',
     license='MIT',
     author='J.Renero',
     author_email='',
-    description='Simplify ML tasks with datasets to Python newcomers'
+    description='Simplify ML tasks with datasets to Python newcomers',
+    install_requires=['m2r']
 )

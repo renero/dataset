a7771f10c72c2f1a9db0453d4224592e455c5a2a
jesus.renero <jesus.renero@gmail.com>
2021-07-19 15:50:34 +0200

    easier print_cm

--- dataset/utils.py
+++ dataset/utils.py
@@ -99,37 +99,73 @@ def find_best_factor(number):
     return sorted(p, reverse=True)
 
 
-def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False,
-             hide_threshold=None):
-    """pretty print for confusion matrixes"""
-    columnwidth = max([len(x) for x in labels] + [5])  # 5 is value length
-    empty_cell = " " * columnwidth
+from typing import List, Optional
 
-    # Begin CHANGES
-    fst_empty_cell = (columnwidth - 3) // 2 * " " + "t/p" + (
-                columnwidth - 3) // 2 * " "
+import numpy as np
+from sklearn.metrics import confusion_matrix
 
-    if len(fst_empty_cell) < len(empty_cell):
-        fst_empty_cell = " " * (
-                    len(empty_cell) - len(fst_empty_cell)) + fst_empty_cell
+
+def print_confusion_matrix(
+        y_true: np.ndarray,
+        y_pred: np.ndarray,
+        labels: Optional[List] = None,
+        hide_zeroes: bool = False,
+        hide_diagonal: bool = False,
+        hide_threshold: Optional[float] = None,
+):
+    """Print a nicely formatted confusion matrix with labelled rows and columns.
+
+    Predicted labels are in the top horizontal header, true labels on the
+        vertical header.
+
+    Args:
+        y_true (np.ndarray): ground truth labels
+        y_pred (np.ndarray): predicted labels
+        labels (Optional[List], optional): list of all labels. If None, then
+            all labels present in the data are displayed. Defaults to None.
+        hide_zeroes (bool, optional): replace zero-values with an empty cell.
+            Defaults to False.
+        hide_diagonal (bool, optional): replace true positives (diagonal) with
+            empty cells. Defaults to False.
+        hide_threshold (Optional[float], optional): replace values below this
+            threshold with empty cells. Set to None
+            to display all values. Defaults to None.
+    """
+    if labels is None:
+        labels = np.unique(np.concatenate((y_true, y_pred)))
+    cm = confusion_matrix(y_true, y_pred, labels=labels)
+    # find which fixed column width will be used for the matrix
+    columnwidth = max(
+        [len(str(x)) for x in labels] + [5]
+    )  # 5 is the minimum column width, otherwise the longest class name
+    empty_cell = ' ' * columnwidth
+
+    # top-left cell of the table that indicates that top headers are predicted
+    # classes, left headers are true classes
+    padding_fst_cell = (columnwidth - 3) // 2  # double-slash is int division
+    fst_empty_cell = padding_fst_cell * ' ' + 't/p' + ' ' * (
+                columnwidth - padding_fst_cell - 3)
+
     # Print header
-    print("    " + fst_empty_cell, end=" ")
-    # End CHANGES
-
+    print('    ' + fst_empty_cell, end=' ')
     for label in labels:
-        print("%{0}s".format(columnwidth) % label, end=" ")
+        print(f'{label:{columnwidth}}',
+              end=' ')  # right-aligned label padded with spaces to columnwidth
 
-    print()
+    print()  # newline
     # Print rows
-    for i, label1 in enumerate(labels):
-        print("    %{0}s".format(columnwidth) % label1, end=" ")
+    for i, label in enumerate(labels):
+        print(f'    {label:{columnwidth}}',
+              end=' ')  # right-aligned label padded with spaces to columnwidth
         for j in range(len(labels)):
-            cell = "%{0}.1f".format(columnwidth) % cm[i, j]
+            # cell value padded to columnwidth with spaces and displayed with
+            # one decimal
+            cell = f'{cm[i, j]:{columnwidth}.1f}'
             if hide_zeroes:
                 cell = cell if float(cm[i, j]) != 0 else empty_cell
             if hide_diagonal:
                 cell = cell if i != j else empty_cell
             if hide_threshold:
                 cell = cell if cm[i, j] > hide_threshold else empty_cell
-            print(cell, end=" ")
+            print(cell, end=' ')
         print()
